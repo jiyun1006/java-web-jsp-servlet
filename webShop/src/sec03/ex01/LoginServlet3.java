4b3209877ee4d7a098b2e310ccf99f2a7baa48bb
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-01-17 19:01:07 +0900

    form method - get, post 두 요청 다 받기.
    servlet에서 form 태그의 get, post 메서드의 요청을 다 받기 위해서,
    따로 공용 메서드를 하나 만들어서, 각각 실행하게 한다.

---
+++ webShop/src/sec03/ex01/LoginServlet3.java
@@ -0,0 +1,61 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet3
+ */
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet 메서드 호출");
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost 메서드 호출");
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException
+	{
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("doHandle메서드 호출");
+		System.out.println("아이디 : " + user_id);
+		System.out.println("비밀번호 : " + user_pw);
+	}
+
+}
