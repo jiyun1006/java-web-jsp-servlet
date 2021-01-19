5beffa57c54751157f0bfdda9776fc59f19808b9
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-01-20 01:35:22 +0900

    load-on-startup
    최초 요청에 대한 실행 시간을 절약하기 위해 톰캣 컨테이너 실행 시, 서블릿을 미리 실행한다.

---
+++ pro08/src/sec06/ex01/LoadAppConfig.java
@@ -0,0 +1,57 @@
+package sec06.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoadAppConfig
+ */
+@WebServlet(name = "loadConfig", urlPatterns = { "/loadConfig" }, loadOnStartup = 1)
+public class LoadAppConfig extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ServletContext context;
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LoadAppConfig의 init 메서드 호출");
+		context = config.getServletContext();
+		String menu_member = context.getInitParameter("menu_member");
+		String menu_order = context.getInitParameter("menu_order");
+		String menu_goods = context.getInitParameter("menu_goods");
+		context.setAttribute("menu_member",  menu_member);
+		context.setAttribute("menu_order", menu_order);
+		context.setAttribute("menu_goods", menu_goods);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String menu_member = (String) context.getInitParameter("menu_member");
+		String menu_order = (String) context.getInitParameter("menu_order");
+		String menu_goods = (String) context.getInitParameter("menu_goods");
+		
+		out.print("<html><body>");
+		out.print("<table border=1 cellspacing=0><tr>메뉴 이름</tr>");
+		out.print("<tr><td>" + menu_member + "</td></tr>");
+		out.print("<tr><td>" + menu_order + "</td></tr>");
+		out.print("<tr><td>" + menu_goods + "</td></tr>");
+		out.print("/table></body></html>");
+	}
+
+}
