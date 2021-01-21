ab33e1578bde607706271c27eb2a9cc8e67fceec
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-01-21 22:57:31 +0900

    session 이용해서 로그인 설정
    session에서 최초 session, 이미 존재하는 session을 이용해서 로그인 설정을 했음.

--- pro09/src/sec01/ex02/SecondServlet.java
+++ pro09/src/sec01/ex02/SecondServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SecondServlet
  */
-@WebServlet("/second")
+//@WebServlet("/second")
 public class SecondServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
