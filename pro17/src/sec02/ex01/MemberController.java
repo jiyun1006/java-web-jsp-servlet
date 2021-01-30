a8b3075f01e4466c283283e94976c5a28c310cb7
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-01-31 00:17:57 +0900

    모델2방식 MVC패턴
    모델2방식을 이용한 MVC패턴으로 회원 정보 수정 삭제 기능 구현

---
+++ pro17/src/sec02/ex01/MemberController.java
@@ -0,0 +1,99 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberController
+ */
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	MemberDAO memberDAO;
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		memberDAO = new MemberDAO();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String nextPage = null;
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charsert=utf-8");
+		String action = request.getPathInfo();
+		System.out.println("action:" + action);
+		
+		if (action==null || action.equals("/listMembers.do")) {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage = "/test02/listMembers.jsp";
+			
+		}else if (action.equals("/addMember.do")) {
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO memberVO = new MemberVO(id,pwd,name,email);
+			memberDAO.addMember(memberVO);
+			nextPage = "/member/listMembers.do";
+		}else if (action.equals("/memberForm.do")) {
+			nextPage = "/test02/MemberForm.jsp";
+		}else if (action.equals("/modMemberForm.do")) {
+			String id = request.getParameter("id");
+			MemberVO memInfo = memberDAO.findMember(id);
+			request.setAttribute("memInfo", memInfo);
+			nextPage = "/test02/modMemberForm.jsp";
+		}else if (action.equals("/modMember.do")) {
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			MemberVO memberVO = new MemberVO(id,pwd,name,email);
+			memberDAO.modMember(memberVO);
+			request.setAttribute("msg", "modified");
+			nextPage = "/member/listMembers.do";
+		}else if (action.equals("/delMember.do")) {
+			String id = request.getParameter("id");
+			memberDAO.delMember(id);
+			request.setAttribute("msg", "deleted");
+			nextPage="/member/listMembers.do";
+		}
+		else {
+			List<MemberVO> membersList = memberDAO.listMembers();
+			request.setAttribute("membersList", membersList);
+			nextPage ="/test02/listMembers.jsp";
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response);
+		
+	}
+}
